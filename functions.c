f2d7a5f39e83c3834120b8ee165fe540e28ebb93
Mapu456 <mpaula456@hotmail.com>
2021-09-21 21:33:31 -0500

    pall, pint, pop and swap functions

--- functions.c
+++ functions.c
@@ -45,29 +45,54 @@ void _push(stack_t **head, unsigned int line_number)
 void _pall(stack_t **head, unsigned int line_number)
 {
 	(void) line_number;
-	while (*head)
+	stack_t *temp = *head;
+
+	while(temp)
 	{
-		printf("%d\n", (*head)->n);
-		*head = (*head)->next;
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 }
 /* pint function */
 void _pint(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-    (void) line_number;
+	if (*head == NULL)
+	{
+		printf("L%u: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
 }
 /* pop function */
 void _pop(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-    (void) line_number;
+	stack_t *delete_node;
+
+	if (head == NULL || *head == NULL)
+	{
+		printf("L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	delete_node = *head;
+	*head = delete_node->next;
+	if (*head != NULL)
+	{
+		(*head)->prev = NULL;
+	}
+	free(delete_node);
 }
 /* swap function */
 void _swap(stack_t **head, unsigned int line_number)
 {
-	(void) head;
-    (void) line_number;
+	int i = (*head)->n;
+
+	if ((*head)->next == NULL)
+	{
+		printf("L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*head)->n = (*head)->next->n;
+	(*head)->next->n = i;
 }
 /* add function */
 void _add(stack_t **head, unsigned int line_number)
@@ -81,3 +106,4 @@ void _nop(stack_t **head, unsigned int line_number)
 	(void) head;
     (void) line_number;
 }
+
